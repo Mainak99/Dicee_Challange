2f3668c34609dd83de727cf7c1ee7f85f7784bcf
Mainak Biswas <mainakbiswas64@gmail.com>
2022-02-01 16:12:01 +0530

    After watching the solution

--- index.js
+++ index.js
@@ -23,35 +23,3 @@ document.querySelector("h1").innerHTML=winner;
 console.log(player1);
 console.log(player2);
 console.log(winner);
-
-
-
-// function player1dice(){
-//     if (player1 === 1)
-//     document.querySelector(".img1").setAttribute("src","images/dice1.png");
-//     else if (player1 === 2)
-//     document.querySelector(".img1").setAttribute("src","images/dice2.png");
-//     else if (player1 === 3)
-//     document.querySelector(".img1").setAttribute("src","images/dice3.png");
-//     else if (player1 === 4)
-//     document.querySelector(".img1").setAttribute("src","images/dice4.png");
-//     else if (player1 === 5)
-//     document.querySelector(".img1").setAttribute("src","images/dice5.png");
-//     else // (player1 === 6)
-//     document.querySelector(".img1").setAttribute("src","images/dice6.png");
-       
-// }
-// function player2dice(){
-//     if (player2 === 1)
-//     document.querySelector(".img2").setAttribute("src","images/dice1.png");
-//     else if (player2 === 2)
-//     document.querySelector(".img2").setAttribute("src","images/dice2.png");
-//     else if (player2 === 3)
-//     document.querySelector(".img2").setAttribute("src","images/dice3.png");
-//     else if (player2 === 4)
-//     document.querySelector(".img2").setAttribute("src","images/dice4.png");
-//     else if (player2 === 5)
-//     document.querySelector(".img2").setAttribute("src","images/dice5.png");
-//     else // (player2 === 6)
-//     document.querySelector(".img2").setAttribute("src","images/dice6.png");
-// }
